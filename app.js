3c3097535419fafc0309d7e5924f961577b839c0
Marco Lopez <mlopez@marostdevelopers.com>
2014-10-07 21:18:00 -0500

    agregando id con numero aleatorio a enlaces a editar
    - mostrar botones de edicion de texto
    - botones de editarSi y editarNo de texto agregado

--- app.js
+++ app.js
@@ -2,50 +2,45 @@ $(function(){
 	$("#enlace").hide();
 	$("#editarNo").hide();
 	$("#editarSi").show();
-
-	
-
 	
 	$("#estilos-texto a").on("click", function(){
 		var estilo = $(this).attr("id");
-		$(".texto span").removeClass();
-		$(".texto span").addClass("tp-caption "+estilo);
+		$(".texto div").removeClass();
+		$(".texto div").addClass("tp-caption "+estilo);
 	});
 
 	$("#agregar").on("click", function(){
 		var aleatorio = Math.floor(Math.random() * 999985) + 15;
 
-		var TextoContenido = '<div id="'+aleatorio+'" class="texto"><span>Texto</span><a class="candado unlock" href="javascript:;"><i class="fa fa-unlock"></i></a><a class="editar" href="javascript:;"><i class="fa fa-pencil"></i></a></div>';		
+		var TextoContenido = '<div id="'+aleatorio+'" class="texto"><div>Texto</div><a class="candado unlock" href="javascript:;"><i class="fa fa-unlock"></i></a><a id="'+aleatorio+'" class="editarSi" href="javascript:;"><i class="fa fa-pencil"></i> EditarSi</a><a id="'+aleatorio+'" class="editarNo" href="javascript:;"><i class="fa fa-pencil"></i> EditarNo</a></div>';
 
 		$("#contenido-texto").append(TextoContenido);
 
+		//OCULTAR BOTONES
+		$(".editarNo").hide();
+		$(".editarSi").show();
+
 		//ARRASTRAR
-		$('.texto').draggable();
+		$('.texto').draggable({disabled:false});
 
 		//EDITAR TEXTO
-		/*
-		$(".editar").on("click", function(){
-			//$(this).hide();
-			//$("#editarNo").show();
-			$(".texto span").attr("contenteditable", "true");
-		});*/
-
-		$('.editar').toggle(
-	    	function () {
-                $(".texto span").attr("contenteditable", "true");
-            },
-	    	function () {
-                $(".texto span").attr("contenteditable", "false");
-            }
-        );
+		$(".editarSi").on("click", function(){
+			var id = $(this).attr("id");
+			$(this).hide();
+			$("#"+id+".editarNo").show();
+			$('div#'+id).draggable({disabled:true});
+			$("#"+id+" div").attr("contenteditable", "true");
+		});
 
 		//NO EDITAR TEXTO
-/*
-		$("#editarNo").on("click", function(){
+		$(".editarNo").on("click", function(){
+			var id = $(this).attr("id");
 			$(this).hide();
+			$("#"+id+".editarSi").show();
 			$("#editarSi").show();
-			$(".texto span").attr("contenteditable", "false");
-		});*/
+			$('div#'+id).draggable({disabled:false});
+			$("#"+id+" div").attr("contenteditable", "false");
+		});
 
 	});
 
@@ -57,7 +52,7 @@ $(function(){
 	});
 
 	$("#enviar").on("click", function(){
-		var TextoEstilo = $(".texto span").attr("class");;
+		var TextoEstilo = $(".texto div").attr("class");;
 		TextoX = texto.position().left;
 	    TextoY = texto.position().top;
 
