17cbc16eecbec4f043c169a1b40435da14727c17
Marco Lopez <mlopez@marostdevelopers.com>
2014-10-10 15:34:30 -0500

    script de Revolution Slider en editor

--- index.php
+++ index.php
@@ -10,31 +10,166 @@
 	<!-- Font Awesome Icon -->
 	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 
+	<!-- jQuery -->
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.js"></script>
+
 	<!-- jQuery UI CSS -->
-	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.1/themes/smoothness/jquery-ui.css">
+	<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/themes/smoothness/jquery-ui.css" />
+	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js"></script>
 
 	<!-- Estilos -->
 	<link rel="stylesheet" href="estilos.css">
-
-	<!-- Estilos Revolution Slider -->
-	<link rel="stylesheet" href="rs-plugin/css/settings.css">
-
+	
+	<!-- APP -->
+	<script type="text/javascript" src="app.js"></script>
+	
 	<!-- SPECTRUM - COLORPICKER -->
 	<link rel="stylesheet" href="http://bgrins.github.io/spectrum/spectrum.css">
+	<script src="http://bgrins.github.io/spectrum/spectrum.js"></script>
+
+	<!-- #### Revolution Slider #### -->
+	<link rel="stylesheet" href="rs-plugin/css/settings.css">
+
+    <!-- CSS STYLE-->
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+
+    <!-- THE PREVIEW STYLE SHEETS, NO NEED TO LOAD IN YOUR DOM -->
+    <link rel="stylesheet" type="text/css" href="css/noneed.css" media="screen" />
+
+    <!-- SLIDER REVOLUTION 4.x SCRIPTS  -->    
+     <script type="text/javascript" src="rs-plugin/js/jquery.themepunch.tools.min.js"></script>   
+    <script type="text/javascript" src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+
+	<!-- SLIDER REVOLUTION 4.x CSS SETTINGS -->
+    <link rel="stylesheet" type="text/css" href="css/extralayers.css" media="screen" />	
+	<link rel="stylesheet" type="text/css" href="rs-plugin/css/settings.css" media="screen" />
+
+	<script type="text/javascript">
+
+	var jSlider = jQuery.noConflict();
+
+	jSlider(document).ready(function(){
+								
+		jSlider('.tp-banner').show().revolution(
+		{
+			dottedOverlay:"none",
+			delay:16000,
+			startwidth:1170,
+			startheight:500,
+			hideThumbs:200,
+			
+			thumbWidth:100,
+			thumbHeight:50,
+			thumbAmount:5,
+			
+			navigationType:"bullet",
+			navigationArrows:"solo",
+			navigationStyle:"preview4",
+			
+			touchenabled:"on",
+			onHoverStop:"on",
+			
+			swipe_velocity: 0.7,
+			swipe_min_touches: 1,
+			swipe_max_touches: 1,
+			drag_block_vertical: false,
+									
+									parallax:"mouse",
+			parallaxBgFreeze:"on",
+			parallaxLevels:[7,4,3,2,5,4,3,2,1,0],
+									
+			keyboardNavigation:"off",
+			
+			navigationHAlign:"center",
+			navigationVAlign:"bottom",
+			navigationHOffset:0,
+			navigationVOffset:20,
+
+			soloArrowLeftHalign:"left",
+			soloArrowLeftValign:"center",
+			soloArrowLeftHOffset:20,
+			soloArrowLeftVOffset:0,
+
+			soloArrowRightHalign:"right",
+			soloArrowRightValign:"center",
+			soloArrowRightHOffset:20,
+			soloArrowRightVOffset:0,
+					
+			shadow:0,
+			fullWidth:"off",
+			fullScreen:"on",
+
+			spinner:"spinner4",
+			
+			stopLoop:"off",
+			stopAfterLoops:-1,
+			stopAtSlide:-1,
+
+			shuffle:"off",
+			
+			autoHeight:"off",						
+			forceFullWidth:"off",						
+									
+									
+									
+			hideThumbsOnMobile:"off",
+			hideNavDelayOnMobile:1500,						
+			hideBulletsOnMobile:"off",
+			hideArrowsOnMobile:"off",
+			hideThumbsUnderResolution:0,
+			
+			hideSliderAtLimit:0,
+			hideCaptionAtLimit:0,
+			hideAllCaptionAtLilmit:0,
+			startWithSlide:0,
+			fullScreenOffsetContainer: ".header"	
+		});
+		
+						
+	});	//ready
+
+	</script>
+
+	<!-- #### Revolution Slider #### -->	
 
 </head>
 <body>
 
-<div class="imagen">
-	<img src="images/seabg1.jpg" alt="">
+	<div class="tp-banner-container">
+		<div id="contenido"><div id="contenido-texto"></div></div>
 
-	<div id="contenido">
+		<div class="tp-banner" >
 
-		<div id="contenido-texto"></div>
+			<ul>
 
-	</div>	
+				<li data-transition="fade" data-slotamount="7" data-masterspeed="500" data-saveperformance="on" data-title="Intro Slide">
 
-</div>
+					<!-- MAIN IMAGE -->
+					<img  alt="seabg1" data-lazyload="images/seabg1.jpg" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
+					<!-- LAYERS -->
+
+					<?php for($i=0; $i<count($array); $i++){ ?>
+
+					<!-- LAYER NR. 4 -->
+					<div style="font-size: <?php echo $array[$i]->tamano; ?>px; color: #<?php echo $array[$i]->color; ?>" class="tp-caption tp-resizeme"
+						data-x="<?php echo $array[$i]->x; ?>"
+						data-y="<?php echo $array[$i]->y; ?>" 
+						data-speed="500"
+						data-start="800"
+						data-easing="Power3.easeInOut"><?php echo $array[$i]->texto; ?>
+					</div>
+
+					<?php } ?>				
+
+				</li>
+	
+			</ul>
+
+			<div class="tp-bannertimer"></div>
+
+		</div>
+
+	</div>
 
 <a id="fondoNo" href="javscript:;">OCULTAR TRANSPARENCIA</a>
 <a id="fondoSi" href="javscript:;">MOSTRAR TRANSPARENCIA</a>
@@ -60,18 +195,6 @@
 	</div>
 	
 </div>
-
-<!-- jQuery -->
-<script src="http://code.jquery.com/jquery-1.10.2.js"></script>
-
-<!-- jQuery UI -->
-<script src="http://code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
-
-<!-- APP -->
-<script type="text/javascript" src="app.js"></script>
-
-<!-- SPECTRUM - COLORPICKER -->
-<script src="http://bgrins.github.io/spectrum/spectrum.js"></script>
 
 </body>
 </html>
