b71664db554526dfdfd091cbca2372f3ffca1053
Marco Lopez <mlopez@marostdevelopers.com>
2014-11-25 16:28:19 -0500

    comparación de archivo y devuelve valor de que tan similar es

---
+++ pruebas/nuevo.php
@@ -0,0 +1,9 @@
+<?php
+
+$texto1 = "Hay muchas variaciones de los pasajes de Lorem Ipsum disponibles, pero la mayoría sufrió alteraciones en alguna manera, ya sea porque se le agregó humor, o palabras aleatorias que no parecen ni un poco creíbles.";
+
+$texto2 = "Hay muchas variaciones de los pasajes de Lorem Ipsum disponibles, pero la mayoría sufrió alteraciones en alguna manera, ya sea porque se le agregó humor, o palabras aleatorias que no parecen ni un poco creíbles.";
+
+similar_text($texto1, $texto2, $valor);
+
+echo intval($valor)."%";
